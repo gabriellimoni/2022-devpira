9f48bf24f1bb3cf17a31f7007d6f480c3067a13c
Gabriel Limoni <gabrielblimoni@gmail.com>
2022-11-19 09:20:34 -0300

    feat: add createToolController

--- src/controllers/toolsController.ts
+++ src/controllers/toolsController.ts
@@ -1,16 +1,12 @@
 import { json, Router } from "express";
+import { createToolController } from "../clean/http/controllers/createToolController";
 import { Tool } from "../models/toolModel";
 
 export const toolsRouter = Router();
 
 toolsRouter.post("/tool", json(), async (req, res) => {
-  if (!req.body.name) {
-    return res.status(400).send({
-      message: "Cannot create a tool without name",
-    });
-  }
-  const createdTool = await Tool.query().insertAndFetch(req.body);
-  return res.status(201).send(createdTool);
+  const result = await createToolController(req);
+  return res.status(result.status).send(result.data);
 });
 
 toolsRouter.get("/tool", async (req, res) => {
