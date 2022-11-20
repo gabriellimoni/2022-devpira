a12fa2f00a790b6ba9a65d1e0807b98e250464dc
Gabriel Limoni <gabrielblimoni@gmail.com>
2022-11-20 08:28:11 -0300

    test: add unit tests to createToolController

---
+++ src/clean/http/controllers/__tests__/createToolController.spec.ts
@@ -0,0 +1,78 @@
+import { Tool } from "../../../domain/entities/tool";
+import { CreateToolRepository } from "../../../domain/repositories/toolRepository";
+import { CreateToolController } from "../createToolController";
+
+class MockedCreateToolRepo implements CreateToolRepository {
+  async create(tool: Omit<Tool, "id">): Promise<Tool> {
+    return {
+      id: 10,
+      ...tool,
+    };
+  }
+}
+
+describe("Create Tool Controller", () => {
+  test("Should returtn 400 if name is not sent", async () => {
+    const controller = new CreateToolController(new MockedCreateToolRepo());
+    const result = await controller.handle({
+      body: {
+        name: "",
+      },
+    });
+    expect(result).toEqual({
+      status: 400,
+      data: {
+        message: "Cannot create a tool without name",
+      },
+    });
+  });
+
+  test("Should call createToolRepo with correct params - without tags", async () => {
+    const createToolRepo = new MockedCreateToolRepo();
+    const controller = new CreateToolController(createToolRepo);
+    const createToolSpy = jest.spyOn(createToolRepo, "create");
+    await controller.handle({
+      body: {
+        name: "Any name",
+      },
+    });
+    expect(createToolSpy).toHaveBeenCalledWith({
+      name: "Any name",
+    });
+  });
+
+  test("Should call createToolRepo with correct params - with tags", async () => {
+    const createToolRepo = new MockedCreateToolRepo();
+    const controller = new CreateToolController(createToolRepo);
+    const createToolSpy = jest.spyOn(createToolRepo, "create");
+    await controller.handle({
+      body: {
+        name: "Any name",
+        tags: ["tag1"],
+      },
+    });
+    expect(createToolSpy).toHaveBeenCalledWith({
+      name: "Any name",
+      tags: ["tag1"],
+    });
+  });
+
+  test("Should return 201 and created tool correctly", async () => {
+    const createToolRepo = new MockedCreateToolRepo();
+    const controller = new CreateToolController(createToolRepo);
+    const result = await controller.handle({
+      body: {
+        name: "Any name",
+        tags: ["tag1"],
+      },
+    });
+    expect(result).toEqual({
+      status: 201,
+      data: {
+        name: "Any name",
+        tags: ["tag1"],
+        id: expect.any(Number),
+      },
+    });
+  });
+});
